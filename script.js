d9a59325a752878feabd113fffa844f52b0f20b4
olivernjiru <olivernjiru@gmail.com>
2021-12-28 23:53:47 +0300

    hamburger creation attempt number 1

---
+++ script.js
@@ -0,0 +1,12 @@
+const btn = document.getElementById('menu-btn')
+const nav = document.getElementById('menu')
+
+function navToggle() {
+    // Toggles for the open class when the button is clicked //
+    btn.classList.toggle('open')
+    nav.classList.toggle('hidden')
+    // To prevent scrolling the rest of the page while the Hamburger Menu is open //
+    document.body.classList.toggle('no-scroll')
+}
+
+btn.addEventListener('click', navToggle)
