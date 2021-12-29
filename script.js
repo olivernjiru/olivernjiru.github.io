66fff7ba9b338503df0469ba91d248bbe5712f01
olivernjiru <olivernjiru@gmail.com>
2021-12-29 08:26:29 +0300

    fixed hamburger functionality :)

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const nav = document.getElementById('menu')
 function navToggle() {
     // Toggles for the open class when the button is clicked //
     btn.classList.toggle('open')
-    nav.classList.toggle('hidden')
+    nav.classList.toggle('mobile-menu-hidden')
     // To prevent scrolling the rest of the page while the Hamburger Menu is open //
     document.body.classList.toggle('no-scroll')
 }
