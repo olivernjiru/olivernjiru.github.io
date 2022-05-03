ec4feb89b12aecc79887924ee4c310a1e59c6c02
Oliver Njiru <olivernjiru@gmail.com>
2022-05-03 16:30:22 +0300

    added defer attribute

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-const btn = document.getElementById('menu-btn')
-const nav = document.getElementById('menu')
 
-function navToggle() {
-    // Toggles for the open class when the button is clicked //
-    btn.classList.toggle('open')
-    nav.classList.toggle('mobile-menu-hidden')
-    // To prevent scrolling the rest of the page while the Hamburger Menu is open //
-    document.body.classList.toggle('no-scroll')
-}
-
-btn.addEventListener('click', navToggle)
